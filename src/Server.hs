e4f5966e17b4163a0be5b2f7bef31e7245c1ccc2
Christophe Thibaut <cthibaut@octo.com>
2021-06-18 18:40:06 +0200

    pseudo login, using IORef as state

--- src/Server.hs
+++ src/Server.hs
@@ -7,13 +7,27 @@
 module Server where
 
 import Control.Applicative
+import Control.Monad.IO.Class
+import Data.IORef
+import Data.Maybe
 import Data.Text
 import Yesod
 import Yesod.Form.Jquery
 import Yesod.Static
 
+type UserId = Text
+data User = User
+    { userId  :: UserId
+    , userPassword :: Text }
+    deriving (Eq, Show)
 
-data HelloWorld = HelloWorld { getStatic :: Static }
+data HelloWorld = HelloWorld { getStatic :: Static
+                             , userRef   :: IORef (Maybe UserId) }
+
+appUser :: HelloWorld -> IO UserId
+appUser app = do
+    the_user_id <- readIORef (userRef app)
+    return $ maybe "" id the_user_id
 
 staticFiles "static"
 
@@ -29,11 +43,6 @@ instance RenderMessage HelloWorld FormMessage where
 
 instance YesodJquery HelloWorld
 
-data User = User
-    { userId  :: Text
-    , userPassword :: Text }
-    deriving Show
-
 userForm :: Html -> MForm Handler (FormResult User, Widget)
 userForm = renderDivs $ User
     <$> areq textField "Id" Nothing
@@ -42,22 +51,34 @@ userForm = renderDivs $ User
 
 getHomeR :: Handler Html
 getHomeR = do
+    state <- getYesod
+    user_id <- liftIO $ appUser state
     (formWidget, enctype) <- generateFormPost userForm
     defaultLayout $ do
         addStylesheet (StaticR css_main_css)
         [whamlet|
             <p> Hello world!
-            <form method=post action=@{HomeR} enctype=#{enctype}>
-                ^{formWidget}
-                <button>Login
+            $if (user_id /= "")
+                <p>Welcome #{user_id} !
+            $else
+                <form method=post action=@{HomeR} enctype=#{enctype}>
+                    ^{formWidget}
+                    <button>Login
 |]
 
 postHomeR :: Handler Html
-postHomeR = defaultLayout [whamlet|
-    <p> Hello world!
-|]
+postHomeR = do
+    ((result, widget), enctype) <- runFormPost userForm
+    state <- getYesod
+    let ref = userRef state
+    case result of
+      FormSuccess user -> do
+          liftIO $ writeIORef ref (Just (userId user))
+          liftIO $ print ((userId user) <> " (pseudo) logged in")
+    redirect HomeR
 
 serve :: IO ()
 serve = do
     static <- staticDevel "static"
-    warp 3000 $ HelloWorld { getStatic = static }
+    ref <- liftIO $ newIORef Nothing
+    warp 3000 $ HelloWorld { getStatic = static, userRef = ref }
